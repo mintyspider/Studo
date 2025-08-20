107bbc45b7ee38f1e962d8ce8d58b6e299221de3
Asya Larionova <m1ntyspider@yandex.ru>
2025-08-20 10:32:45 +0300

    Иконка сайта

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -63,6 +63,7 @@ export default defineConfig({
     ['script', {
       id: 'MathJax-script',
       src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
-    }]
+    }],
+    ['link', { rel: 'icon', href: '/logo.png' }]
   ]
 });
