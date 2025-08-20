07f7897a1633420ea6f6ae01b1de85120a14b8dc
Asya Larionova <m1ntyspider@yandex.ru>
2025-08-20 10:25:37 +0300

    Добавление логотипа

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,26 +4,50 @@ import { navigation } from "./navigation";
 import mathjax3 from 'markdown-it-mathjax3';
 
 export default defineConfig({
+  lang: 'ru',
   title: "StuDo",
   description: "База знаний ИСиТ ПетрГУ",
   base: "/Studo/",
   themeConfig: {
-    editLink: {
-      pattern: "https://github.com/mintyspider/Studo/edit/main/docs/:path",
-      text: "Редактировать на GitHub",
-    },
+    logo: '/logo.png',
     socialLinks: [
       {
         icon: "github",
         link: "https://github.com/mintyspider/Studo",
       },
     ],
+    docFooter: {
+      prev: 'Предыдущая страница',
+      next: 'Следующая страница'
+    },
     sidebar,
     nav: navigation,
     footer: {
       message: "Студенты ИСиТ",
       copyright: "2025 ©mintyspider",
     },
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: 'Поиск',
+            buttonAriaLabel: 'Поиск',
+          },
+          modal: {
+            displayDetails: 'Отобразить подробный список',
+            resetButtonTitle: 'Сбросить поиск',
+            backButtonTitle: 'Закрыть поиск',
+            noResultsText: 'Нет результатов по запросу',
+            footer: {
+              selectText: 'Выбрать',
+              navigateText: 'Перейти',
+              closeText: 'Закрыть'
+            }
+          }
+        }
+      }
+    }
   },
   cleanUrls: true,
   markdown: {
