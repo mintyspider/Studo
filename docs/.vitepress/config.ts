10f49680d430c5c12b529c1fcd6da9e0e810eae2
Asya Larionova <m1ntyspider@yandex.ru>
2025-08-20 10:37:47 +0300

    лого для сайта

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -64,6 +64,6 @@ export default defineConfig({
       id: 'MathJax-script',
       src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
     }],
-    ['link', { rel: 'icon', href: '/logo.png' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
   ]
 });
